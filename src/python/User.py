3b4348476caa5b7d630dcca6e4742353c6de4907
MrnsXns <65809489+MrnsXns@users.noreply.github.com>
2020-05-28 00:28:22 +0300

    Update User.py

--- src/python/User.py
+++ src/python/User.py
@@ -19,9 +19,7 @@ class User():
 
         
     
-
-
-    #def friendReq(s
+    
     def showListEvents(self,userID):
         events=[]
         val=(userID)
@@ -129,7 +127,7 @@ class User():
             mycursor.close()
 
     def acceptFriendReq(self,from_usrname):
-        #Find from_id from from_username
+        #Find -from_id- from -from_username-
         val=(from_usrname)
         mycursor = mydb.cursor()
         mycursor.execute("select userID from _user where usrname= %s",(val,))
@@ -139,12 +137,24 @@ class User():
         #print(to_userID)
         mycursor.close()
 
+        #update table buddy_req
         val_=(from_userID,str(self.userID))
         mycursor_1 = mydb.cursor()
         mycursor_1.execute("update buddy_req set is_accepted=1 where from_user= %s and to_user= %s ",val_)
         mydb.commit()
         print(mycursor.rowcount, "record(s) affected")
         mycursor_1.close()
+
+        #update table buddies
+        val_2 =(self.userID,from_userID)
+        mycursor_2 = mydb.cursor()
+        mycursor_2.execute("insert into buddies (userID,buddy) values (%s, %s)",val_2)
+        mydb.commit()
+        print(mycursor.rowcount, "A new friendship begins...")
+        mycursor_2.close()
+        
+
+        
 
     def denyFriendReq(self,from_usrname):
             #Find from_id from from_username
@@ -165,17 +175,86 @@ class User():
             mycursor_1.close()
 
 
+    def getBuddiesID(self):
+        buddies_list=[]
+        val=(self.userID)
+        mycursor = mydb.cursor()
+        mycursor.execute("select buddy from buddies where buddies.userID= %s ",(val,))
+        myresult = mycursor.fetchall()
+        #print(myresult)
+        for pair in myresult:
+            for buddyID in pair:
+                buddies_list.append(buddyID)
+        #print(buddies_list)
+        return buddies_list
+        mycursor.close()
+        
+    def showFriendsEvents(self):
+        b=self.getBuddiesID()
+        print(b)
+        #v=("YES")
+        shared={}
+        tuples_usrnms=[]
+        tuples_events=[]
+        for ids in b:
+            mycursor_usrnames = mydb.cursor()
+            mycursor_usrnames.execute("select usrname from _user,_event where  _event.userID=_user.userID and _event.shared='YES' and _user.userID="+str(ids))
+            myresult_usrnames = mycursor_usrnames.fetchall()    
+            #print(myresult_usrnames)
+            tuples_usrnms.append(myresult_usrnames)
+            
+            
+            mycursor_events=mydb.cursor()
+            mycursor_events.execute("select descr, start_date, _end_date from _event where shared='YES' and start_date <= now() and userID="+str(ids))
+            myresult_events = mycursor_events.fetchall()
+            #print(myresult_events)
+            tuples_events.append(myresult_events)
+        
+        
+        
+        #print(tuples_usrnms)
+        #print(tuples_events)
+        f_usrnms=[]
+        f_events=[]
+        
+        for pair in tuples_usrnms:
+            for usrname in pair:
+                f_usrnms.append(usrname)
+        print(f_usrnms)
 
+        for pair in tuples_events:
+            for event in pair:
+                f_events.append(event)
+        print(f_events)
+
+        for i in range(len(f_events)):
+            #print(f_events[i][2])
+            return str(f_usrnms[i][0])#User's username
+            return str(f_events[i][0])#event's description
+            return f_events[i][1].year#event's starting date
+            return f_events[i][1].month
+            return f_events[i][1].day
+            return f_events[i][1].hour
+            return f_events[i][1].minute
+            return f_events[i][2].year#event's expired date
+            return f_events[i][2].month
+            return f_events[i][2].day
+            return f_events[i][2].hour
+            return f_events[i][2].minute
+            
         
     
+'''for testing
 mydb= mysql.connector.connect(
         host="localhost",
         user="root",
         passwd="pasatempo64",
         database="whatnau")
 
-'''user1=User(4)#Bobos
-user2=User(3)#jason
+user1=User(1)#Bobos
+user2=User(3)
 #user2.sendFriendReq("Bobos")
-user1.denyFriendReq("Marinero")'''
+user1.showFriendsEvents()
 mydb.close()
+'''
+

41593e8172bb7ac9cd6b0ad5083c7d30b45e93b5
MrnsXns <65809489+MrnsXns@users.noreply.github.com>
2020-05-27 17:50:35 +0300

    Update whatnau .sql

--- whatnau .sql
+++ whatnau .sql
@@ -8,7 +8,7 @@ userID bigint(16)  auto_increment,
 email varchar(128),
 usrname varchar(128),
 pass varchar(128),
-cr_cardID bigint(20) ,
+cr_cardID bigint(20)  ,
 interests set("sports","music","theater","cinema","politics","underground","festivals","fashion","undefined") default "undefined",
 gender enum("M","F"),/*M=male , F=female*/
 _type enum("O","SU"),/*O=organiser , SU=simple_user*/
@@ -20,6 +20,17 @@ primary key(userID)
 
 
 )engine=InnoDB;
+
+create table buddy_req(
+reqID bigint(16)  auto_increment, 
+from_user bigint(16), 
+to_user bigint(16), 
+is_accepted int(3) default 0,/* 0=under processing , 1=request accepted , 2=request rejected*/
+primary key(reqID),
+
+constraint FRIENDSHIP_REQ foreign key (from_user) references _user(userID) on delete cascade on update cascade
+)engine=InnoDB;
+
 
 create table buddies(
 friendshipID bigint(16) not null auto_increment, 
@@ -36,11 +47,11 @@ create table _event(
 
 eventID bigint(16) auto_increment,
 userID bigint(16) not null,
-descr text,
+descr text ,
 _type set("sports",'social','arts','business','study','food','workout',"travelling","undefined") default "undefined",
 importance enum("LOW","MED","HIGH") default "HIGH",
-start_date datetime,
-_end_date datetime,
+start_date datetime default null,
+_end_date datetime default null,
 shared enum("YES","NO") default "NO",
 
 primary key(eventID),
@@ -83,7 +94,10 @@ constraint PARTICIPANTS_PROMOTED foreign key (promotedID) references promoted_ev
 
 
 
-/*use whatnau;*/
+
+
+
+
 insert into _user(email,usrname,pass,cr_cardID,gender,_type,job,corporation) values
 ("jason@gmail.com","jason","JJason","54546546","M","SU",null,null),
 ("mxins@freemail.gr","Marinero","you_shall_not_pass",null,"M","SU",null,null),
@@ -100,6 +114,12 @@ insert into _event(userID,descr,_type,importance,start_date,_end_date,shared) va
 (3,"undefined","undefined","MED","2018-02-13 20:23:34","2018-03-13 20:23:34","NO"),
 (2,"basketball","sports","HIGH","2018-02-13 18:23:34","2018-02-13 21:23:34","NO"),
 (4,"concert","arts,social","LOW","2020-02-13 20:23:34","2020-02-13 23:00:00","YES");
+
+insert into buddy_req(from_user,to_user,is_accepted) values 
+(1,4,0),
+(2,4,0),
+(1,3,0),
+(3,4,0);
 
 
 

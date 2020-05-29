06a8eef796bca2ed6d5bb3c555bce80dad0e8489
MrnsXns <65809489+MrnsXns@users.noreply.github.com>
2020-05-30 01:10:15 +0300

    Update main.py

--- src/python/main.py
+++ src/python/main.py
@@ -4,3 +4,18 @@ import eel
 eel.init("../web")
 
 eel.start('month-view/month.html', size=(1000, 562))
+
+def userExists(user_email,user_pass):
+    val=(user_email,user_pass)
+    mycursor = mydb.cursor()
+    mycursor.execute("select userID from _user where email= %s and pass= %s ",val)
+    myresult = mycursor.fetchall()
+    if len(myresult)!=0:
+        print("Welcome user")
+        #print(myresult)
+        return True
+    else:
+        print("Invalid password or email address OR user doesn't exist\nplease try again")
+        #print(myresult)
+        return False
+ 

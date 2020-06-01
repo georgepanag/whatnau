29eb3dc537ce41b3ee7492e850f7bf849e8d20d9
georgepanag <gepanagiot@ceid.upatras.gr>
2020-06-01 21:58:46 +0300

    finalized side panel view

--- src/web/month-view/month.js
+++ src/web/month-view/month.js
@@ -3,6 +3,8 @@ let month = date.getMonth() + 1;
 let month_day = date.getDate();
 let year = date.getYear();
 let user_events_obj = {};
+let months = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
 eel.get_stats()(ret => populate_calendar_data("#week-rows",ret));
 
 
@@ -11,7 +13,6 @@ function populate_calendar_data(dom_id, calendar_data){
 	let user_events = calendar_data.user_events;
 	let idx = 0;
 	let weeks = $(dom_id).children().toArray(); 
-	console.log(user_events)
 	for(week of weeks){
 		let days = week.children;
 		for(day of days){
@@ -27,14 +28,13 @@ function populate_calendar_data(dom_id, calendar_data){
 			if(days_list[idx][0] == month_day && days_list[idx][1] == month){
 				day.classList.add("today");
 			}
-			console.log(days_list[idx]+"");
-			console.log(user_events[days_list[idx]+""]);
-			//if( l > 0){
-			//	let node = document.createElement("DIV");
-			//	let text_node = document.createTextNode(l);
-			//	node.appendChild(text_node);
-			//	day.children[1].appendChild(node);
-			//}
+			let l = user_events[days_list[idx]+""].length
+			if( l > 0){
+				let node = document.createElement("DIV");
+				let text_node = document.createTextNode(l);
+				node.appendChild(text_node);
+				day.children[1].appendChild(node);
+			}
 			idx = idx+1;
 		}
 	}
@@ -43,10 +43,10 @@ function populate_calendar_data(dom_id, calendar_data){
 
 function display_day_panel(){
 	$("#day-side-panel").css("width","600px");
-	$("#day-date-info").children()[0].textContent = this.firstElementChild.textContent;
 	$("#big-trans-back-button").css("visibility","visible")
 	$("#big-trans-back-button").click(hide_day_panel);
 	$("#main").css("filter","blur(0.5em)");
+	update_date_info(this.id);
 }
 
 function hide_day_panel(){
@@ -56,6 +56,12 @@ function hide_day_panel(){
 	$("#main").css("filter","none");
 }
 
+function update_date_info(date){
+	x = date.split(",");
+	$("#day-date-info").children()[0].textContent = x[2];
+	$("#day-date-info").children()[1].firstElementChild.textContent = months[x[1]];
+	$("#day-date-info").children()[1].lastElementChild.textContent = x[0];
+}
 
 
 

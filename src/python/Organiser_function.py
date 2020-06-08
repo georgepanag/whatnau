584743c4f87f7e9c3c4587b4549d66fd4a1e0452
dimprosk <64734346+dimprosk@users.noreply.github.com>
2020-06-08 19:45:01 +0300

    Update Organiser_function.py

--- src/python/Organiser_function.py
+++ src/python/Organiser_function.py
@@ -1,41 +1,45 @@
 def send_to_fans(self,userID,mydb):
         val =userID
-
+        
         mycursor = mydb.cursor()
-        mycursor.execute("select interests  from _user where userID=%s ",(val,))
         
-        myresult = mycursor.fetchone()
-        print (str(myresult[0])) #just checking the result
+        mycursor.execute("select interests  from _user")
         
-        mycursor.close()
-
+        myresult = mycursor.fetchall()
+        #for x in myresult:
+            #print(x) #just checking the result
+            
+       
+        print("________________________")
 
         mycursor2 = mydb.cursor()
-        mycursor2.execute("select category  from promoted_event where userID=%s ",(val,))
+        mycursor2.execute("select category  from promoted_event")
         
-        myresult2 = mycursor2.fetchone()
-        print (str(myresult2[0])) #just checking the result
-        return str(myresult)
-        return str(myresult2)
-        mycursor2.close()
+        myresult2 = mycursor2.fetchall()
+        #for y in myresult2:
+            #print(y) #just checking the result
+        
+        for x in myresult:
+            for y in myresult2:
+        
+                if (x)==(y):
+                    mycursor3 = mydb.cursor()
+                    
+                    sql="update promoted_event set userID=%s "
+                    variable=(val)
+                    mycursor3.execute(sql, variable)
+               
+                
+                    mydb.commit()
+                    print(mycursor3.rowcount, "record(s) affected")
 
-        if (myresult)==(myresult2):
-            mycursor3 = mydb.cursor()
-        
-            sql="update promoted_event set userID = %s"
-            variable=(val, userID)
-            mycursor3.execute(sql, variable)
-       
-        
-            mydb.commit()
-            print(mycursor3.rowcount, "record(s) affected")
-            
-        
-            
-        
-        
-        
-        
+                    mycursor3.close()
+                else:
+                    print("The Elements don't match each other")
+        mycursor.close()    
         mycursor2.close()
-
+        
+         
+        
+       #mydb.close()
         

5b7cc1fc6d984b7809d5dc41dfd5544b915d20d3
dimprosk <64734346+dimprosk@users.noreply.github.com>
2020-06-09 18:49:28 +0300

    Update User.py

--- src/python/User.py
+++ src/python/User.py
@@ -132,6 +132,18 @@ class User():
         else: #not full space
             return 0
         
+     def get_promoted_events(self,category,mydb):
+        val =(category)
+        
+        mycursor = mydb.cursor()
+        
+        mycursor.execute("select * from promoted_event where target_aud= 'only fans' and category LIKE %s",(val,))
+        
+        
+        myresult = mycursor.fetchall()
+        #print (myresult)
+        return (myresult)
+        mycursor.close()    
         
        
         

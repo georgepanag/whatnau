1dfbf00f4a016c888c9c1f2cb12b8e69a6440af1
dimprosk <64734346+dimprosk@users.noreply.github.com>
2020-06-09 18:18:16 +0300

    Update Organiser_function.py

--- src/python/Organiser_function.py
+++ src/python/Organiser_function.py
@@ -1,45 +1,12 @@
-def send_to_fans(self,userID,mydb):
-        val =userID
-        
+def get_promoted_events(self,category,mydb):
+        val =(category)
+        #val2=category
         mycursor = mydb.cursor()
         
-        mycursor.execute("select interests  from _user")
+        mycursor.execute("select * from promoted_event where target_aud= 'only fans' and category LIKE %s",(val,))
+        
         
         myresult = mycursor.fetchall()
-        #for x in myresult:
-            #print(x) #just checking the result
-            
-       
-        print("________________________")
-
-        mycursor2 = mydb.cursor()
-        mycursor2.execute("select category  from promoted_event")
-        
-        myresult2 = mycursor2.fetchall()
-        #for y in myresult2:
-            #print(y) #just checking the result
-        
-        for x in myresult:
-            for y in myresult2:
-        
-                if (x)==(y):
-                    mycursor3 = mydb.cursor()
-                    
-                    sql="update promoted_event set userID=%s "
-                    variable=(val)
-                    mycursor3.execute(sql, variable)
-               
-                
-                    mydb.commit()
-                    print(mycursor3.rowcount, "record(s) affected")
-
-                    mycursor3.close()
-                else:
-                    print("The Elements don't match each other")
-        mycursor.close()    
-        mycursor2.close()
-        
-         
-        
-       #mydb.close()
-        
+        #print (myresult)
+        return (myresult)
+        mycursor.close()
